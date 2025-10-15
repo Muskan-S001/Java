494405bcbc102df93ac6b44b0e9adeb1df489bb4
Muskan <muskan.maths2021@gmail.com>
2025-10-15 09:32:14 +0530

    Create CircleAreaParameter.java

---
+++ Variables/Area/CircleAreaParameter.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    long r = scn.nextLong();
+    long sq = (long)Math.pow(r, 2);
+    long a = 3*sq;
+    long p = 2*3*r;
+    System.out.println(a);
+    System.out.println(p);
+}}
