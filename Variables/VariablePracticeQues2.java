17aa4c7b0aa5c6487d207bb2a79303ff3d8aad68
Muskan <muskan.maths2021@gmail.com>
2025-10-18 23:12:17 +0530

    Create VariablePracticeQues2.java

---
+++ Variables/VariablePracticeQues2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        float var;
+        var = 10.45F;
+        var = 20.55F;
+        System.out.print(var);
+    }
+}
